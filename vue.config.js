15860a177ecdb554e7592311383a93d56ef92859
bolladaniele <bolladaniele@gmail.com>
2020-11-09 10:42:58 +0000

    fix vue config for deploy pipeline

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,10 @@ module.exports = {
         additionalData: `@import "@/assets/variables.scss";`
       }
     }
-  }
+  },
+  publicPath:
+    process.env.HEROKU_DEPLOY === "true" ||
+    process.env.NODE_ENV === "development"
+      ? "/" //This is for deployment to Heroku.
+      : "/genie-ai-test/" //This is for deployment to Github Pages.
 };
